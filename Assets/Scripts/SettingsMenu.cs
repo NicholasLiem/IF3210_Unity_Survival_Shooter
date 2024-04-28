733cbfedb710d941b970f0d57de23d5b71459001
Brianaldo <brianaldophandiarta31@gmail.com>
2024-04-28 22:30:45 +0700

    feat: update username and game difficulty

--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -1,14 +1,39 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Audio;
 
 public class SettingsMenu : MonoBehaviour
 {
+    public TMP_InputField usernameInputField;
     public AudioMixer audioMixer;
+    public TMP_Dropdown gameDifficultyDropdown;
+
+    void Start()
+    {
+        if (GameManagement.Instance != null)
+        {
+            usernameInputField.text = GameManagement.Instance.Username;
+        }
+    }
+
+    public void UpdateUsername()
+    {
+        if (GameManagement.Instance != null)
+        {
+            GameManagement.Instance.Username = usernameInputField.text;
+        }
+    }
 
     public void SetVolume(float volume)
     {
         audioMixer.SetFloat("masterVol", volume);
+    }
+
+    public void UpdateGameDifficulty()
+    {
+        if (GameManagement.Instance != null)
+        {
+            GameManagement.Instance.gameDifficulty = (GameDifficulty) gameDifficultyDropdown.value;
+        }
     }
 }
