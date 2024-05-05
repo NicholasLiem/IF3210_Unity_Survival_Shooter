24430e85e4b3e2aa34a065b15b76155df515ddf2
Nicholas Liem <13521135@std.stei.itb.ac.id>
2024-05-05 21:46:23 +0700

    revert: Gun and Shotgun fix

--- Assets/Scripts/Weapon/Gun.cs
+++ Assets/Scripts/Weapon/Gun.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 using UnityEngine.Events;
-using System;
+using System.Text;
 using UnitySampleAssets.CrossPlatformInput;
 
 namespace Nightmare
@@ -30,15 +30,22 @@ namespace Nightmare
 
         void Awake()
         {
-            shootableMask = LayerMask.GetMask("Shootable");
+            // Create a layer mask for the Shootable layer.
+            shootableMask = LayerMask.GetMask("Shootable", "Floor");
+
+            // Set up the references.
             gunParticles = GetComponent<ParticleSystem>();
             gunLine = GetComponent<LineRenderer>();
             gunAudio = GetComponent<AudioSource>();
             gunLight = GetComponent<Light>();
+            //faceLight = GetComponentInChildren<Light> ();
 
             AdjustGrenadeStock(0);
+
             listener = new UnityAction(CollectGrenade);
+
             EventManager.StartListening("GrenadePickup", CollectGrenade);
+
             StartPausible();
         }
 
@@ -53,93 +60,117 @@ namespace Nightmare
             if (isPaused)
                 return;
 
+            // Add the time since Update was last called to the timer.
             timer += Time.deltaTime;
 
 #if !MOBILE_INPUT
             if (timer >= timeBetweenBullets && Time.timeScale != 0)
             {
+                // If the Fire1 button is being press and it's time to fire...
                 if (Input.GetButton("Fire2") && grenadeStock > 0)
                 {
+                    // ... shoot a grenade.
                     ShootGrenade();
                 }
+
+                // If the Fire1 button is being press and it's time to fire...
                 else if (Input.GetButton("Fire1"))
                 {
+                    // ... shoot the gun.
                     Shoot();
                 }
             }
+
 #else
+            // If there is input on the shoot direction stick and it's time to fire...
             if ((CrossPlatformInputManager.GetAxisRaw("Mouse X") != 0 || CrossPlatformInputManager.GetAxisRaw("Mouse Y") != 0) && timer >= timeBetweenBullets)
             {
+                // ... shoot the gun
                 Shoot();
             }
 #endif
-
+            // If the timer has exceeded the proportion of timeBetweenBullets that the effects should be displayed for...
             if (timer >= timeBetweenBullets * effectsDisplayTime)
             {
+                // ... disable the effects.
                 DisableEffects();
             }
         }
 
+
+        public void DisableEffects()
+        {
+            // Disable the line renderer and the light.
+            gunLine.enabled = false;
+            // faceLight.enabled = false;
+            gunLight.enabled = false;
+        }
+
+
         void Shoot()
         {
+            // Reset the timer.
             timer = 0f;
-            GameEventsManager.instance.playerActionEvents.TriggerShotFired();
+
+            // Play the gun shot audioclip.
             gunAudio.Play();
+
+            // Enable the lights.
             gunLight.enabled = true;
+            // faceLight.enabled = true;
+
+            // Stop the particles from playing if they were, then start the particles.
             gunParticles.Stop();
             gunParticles.Play();
+
+            // Enable the line renderer and set it's first position to be the end of the gun.
             gunLine.enabled = true;
             gunLine.SetPosition(0, transform.position);
 
+            // Set the shootRay so that it starts at the end of the gun and points forward from the barrel.
             shootRay.origin = transform.position;
             shootRay.direction = transform.forward;
 
-            RaycastHit[] hits = Physics.RaycastAll(shootRay, range, shootableMask);
-            Array.Sort(hits, (x, y) => x.distance.CompareTo(y.distance));
-
-            bool hitTarget = false;
-            foreach (var hit in hits)
+            // Perform the raycast against gameobjects on the shootable layer and if it hits something...
+            if (Physics.Raycast(shootRay, out shootHit, range, shootableMask))
             {
-                EnemyHealth enemyHealth = hit.collider.GetComponent<EnemyHealth>();
-                BufferHealth bufferPetHealth = hit.collider.GetComponent<BufferHealth>();
-                if (enemyHealth != null || bufferPetHealth != null)
+                // Try and find an EnemyHealth script on the gameobject hit.
+                EnemyHealth enemyHealth = shootHit.collider.GetComponent<EnemyHealth>();
+                BufferHealth bufferPetHealth = shootHit.collider.GetComponent<BufferHealth>();
+
+                // If the EnemyHealth component exist...
+                if (enemyHealth != null)
                 {
-                    ProcessHit(hit);
-                    hitTarget = true;
-                    break;
+                    // ... the enemy should take damage.
+                    enemyHealth.TakeDamage(damagePerShot, shootHit.point);
                 }
-            }
 
-            if (!hitTarget)
+                // If the BufferHealth component exist
+                if (bufferPetHealth != null)
+                {
+                    bufferPetHealth.TakeDamage(damagePerShot);
+                }
+
+                // Set the second position of the line renderer to the point the raycast hit.
+                gunLine.SetPosition(1, shootHit.point);
+            }
+            // If the raycast didn't hit anything on the shootable layer...
+            else
             {
-                GameEventsManager.instance.playerActionEvents.TriggerShotHit(false);
+                // ... set the second position of the line renderer to the fullest extent of the gun's range.
                 gunLine.SetPosition(1, shootRay.origin + shootRay.direction * range);
             }
         }
 
-        void ProcessHit(RaycastHit hit)
+        private void ChangeGunLine(float midPoint)
         {
-            GameEventsManager.instance.playerActionEvents.TriggerShotHit(true);
-            EnemyHealth enemyHealth = hit.collider.GetComponent<EnemyHealth>();
-            BufferHealth bufferPetHealth = hit.collider.GetComponent<BufferHealth>();
+            AnimationCurve curve = new AnimationCurve();
 
-            if (enemyHealth != null)
-            {
-                enemyHealth.TakeDamage(damagePerShot, hit.point);
-            }
+            curve.AddKey(0f, 0f);
+            curve.AddKey(midPoint, 0.5f);
+            curve.AddKey(1f, 1f);
 
-            if (bufferPetHealth != null)
-            {
-                bufferPetHealth.TakeDamage(damagePerShot);
-            }
-
-            gunLine.SetPosition(1, hit.point);
-        }
-
-        void DisableEffects()
-        {
-            gunLine.enabled = false;
-            gunLight.enabled = false;
+            gunLine.widthCurve = curve;
         }
 
         public void CollectGrenade()
@@ -159,6 +190,9 @@ namespace Nightmare
             timer = timeBetweenBullets - grenadeFireDelay;
             GameObject clone = PoolManager.Pull("Grenade", transform.position, Quaternion.identity);
             EventManager.TriggerEvent("ShootGrenade", grenadeSpeed * transform.forward);
+            //GameObject clone = Instantiate(grenade, transform.position, Quaternion.identity);
+            //Grenade grenadeClone = clone.GetComponent<Grenade>();
+            //grenadeClone.Shoot(grenadeSpeed * transform.forward);
         }
     }
 }
--- Assets/Scripts/Weapon/Shotgun.cs
+++ Assets/Scripts/Weapon/Shotgun.cs
@@ -1,6 +1,8 @@
-using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+
 
 namespace Nightmare
 {
@@ -15,6 +17,7 @@ namespace Nightmare
 
         float timer;
         Ray shootRay = new Ray();
+        RaycastHit shootHit;
         int shootableMask;
         ParticleSystem gunParticles;
         LineRenderer gunLine;
@@ -28,25 +31,33 @@ namespace Nightmare
 
         void Awake()
         {
-            // Set up the references and layer mask
             shootableMask = LayerMask.GetMask("Shootable", "Floor");
             gunParticles = GetComponent<ParticleSystem>();
             gunLine = GetComponent<LineRenderer>();
             gunAudio = GetComponent<AudioSource>();
             gunLight = GetComponent<Light>();
+
             AdjustGrenadeStock(0);
 
             listener = new UnityAction(CollectGrenade);
+
             EventManager.StartListening("GrenadePickup", CollectGrenade);
             StartPausible();
         }
-
         void OnDestroy()
         {
             EventManager.StopListening("GrenadePickup", CollectGrenade);
             StopPausible();
         }
 
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
         void Update()
         {
             if (isPaused)
@@ -65,69 +76,6 @@ namespace Nightmare
             }
         }
 
-        void Shoot()
-        {
-            timer = 0f;
-            gunAudio.Play();
-            gunLight.enabled = true;
-            gunParticles.Stop();
-            gunParticles.Play();
-            gunLine.enabled = true;
-            gunLine.SetPosition(0, transform.position);
-
-            shootRay.origin = transform.position;
-            shootRay.direction = transform.forward;
-
-            RaycastHit[] hits = Physics.RaycastAll(shootRay, range, shootableMask);
-            Array.Sort(hits, (x, y) => x.distance.CompareTo(y.distance));
-
-            bool hitTarget = false;
-            foreach (var hit in hits)
-            {
-                if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Shootable"))
-                {
-                    ProcessHit(hit);
-                    hitTarget = true;
-                    break;
-                }
-            }
-
-            if (!hitTarget)
-            {
-                GameEventsManager.instance.playerActionEvents.TriggerShotHit(false);
-                gunLine.SetPosition(1, shootRay.origin + shootRay.direction * range);
-            }
-        }
-
-        void ProcessHit(RaycastHit hit)
-        {
-            GameEventsManager.instance.playerActionEvents.TriggerShotHit(true);
-            EnemyHealth enemyHealth = hit.collider.GetComponent<EnemyHealth>();
-            BufferHealth bufferPetHealth = hit.collider.GetComponent<BufferHealth>();
-
-            if (enemyHealth != null)
-            {
-                float distance = Vector3.Distance(transform.position, hit.point);
-                int finalDamage = (int)(damagePerShot * (1 - distance / range));
-                enemyHealth.TakeDamage(finalDamage, hit.point);
-            }
-
-            if (bufferPetHealth != null)
-            {
-                float distance = Vector3.Distance(transform.position, hit.point);
-                int finalDamage = (int)(damagePerShot * (1 - distance / range));
-                bufferPetHealth.TakeDamage(finalDamage);
-            }
-
-            gunLine.SetPosition(1, hit.point);
-        }
-
-        void DisableEffects()
-        {
-            gunLine.enabled = false;
-            gunLight.enabled = false;
-        }
-
         public void CollectGrenade()
         {
             AdjustGrenadeStock(1);
@@ -137,6 +85,59 @@ namespace Nightmare
         {
             grenadeStock += change;
             GrenadeManager.grenades = grenadeStock;
+        }
+
+        void Shoot()
+        {
+            timer = 0f;
+
+            gunAudio.Play();
+
+            gunLight.enabled = true;
+
+            gunParticles.Stop();
+            gunParticles.Play();
+
+            gunLine.enabled = true;
+            gunLine.SetPosition(0, transform.position);
+
+            shootRay.origin = transform.position;
+            shootRay.direction = transform.forward;
+
+            if (Physics.Raycast(shootRay, out shootHit, range, shootableMask))
+            {
+                EnemyHealth enemyHealth = shootHit.collider.GetComponent<EnemyHealth>();
+                BufferHealth bufferPetHealth = shootHit.collider.GetComponent<BufferHealth>();
+
+                if (enemyHealth != null)
+                {
+                    float distance = Vector3.Distance(transform.position, shootHit.point);
+                    int finalDamage = (int)(damagePerShot * (1 - distance / range));
+                    Debug.Log("This is final damage " + finalDamage);
+                    enemyHealth.TakeDamage(finalDamage, shootHit.point);
+                }
+
+                // If the BufferHealth component exist
+                if (bufferPetHealth != null)
+                {
+                    float distance = Vector3.Distance(transform.position, shootHit.point);
+                    int finalDamage = (int)(damagePerShot * (1 - distance / range));
+                    bufferPetHealth.TakeDamage(finalDamage);
+                }
+                gunLine.SetPosition(1, shootHit.point);
+            }
+            else
+            {
+                gunLine.SetPosition(1, shootRay.origin + shootRay.direction * range);
+            }
+        }
+
+        public void DisableEffects()
+        {
+            // Disable the line renderer and the light.
+            gunLine.enabled = false;
+            // faceLight.enabled = false;
+            gunLight.enabled = false;
         }
     }
 }
